1ead3ab8397c02b53e3fa62048847473ec0e77df
Brian Crocker <briancrocker81@gmail.com>
2015-07-17 11:37:56 +0100

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RwdStudentApplicationForm::Application.config.secret_key_base = '4505ee87ce832b2ab4017d6d920a67668f1cccdd80b7adb8612c48e2975dcd70d2e5ebc6de50f74cc53b132cf797efb1b8dca496c11cb2417bd3e0db78e6c38a'
